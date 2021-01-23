628633dcc30f49bd42a849de76a9d55760b6a6f1
Kerem Karabulut <ke.kerem3@gmail.com>
2021-01-24 00:15:41 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    // Business Classes
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            //if (customer.)
+            //{
+
+            //}
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.CustomerNumber = "1234";
+            customer1.FirstName = "Kerem";
+            customer1.LastName= "Karabulut";
+            customer1.TcNumber = "1234567890";
+
+            CoorporateCustomer customer2 = new CoorporateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNumber = "54321";
+            customer2.CompanyName = "Kodlama.io";
+            customer2.ExciseNumber = "123456666";
+
+            //Gerçek Müşteri - Tüzel Müşteri
+
+            Customer customer3 = new IndividualCustomer();
+            Customer customer4 = new CoorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+
+                        
+
+        }
+    }
+}
